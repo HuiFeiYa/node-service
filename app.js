6660aea92c494847c952a71b86368db39d57ca98
guanwanxiao <18257132004@163.com>
2019-06-18 16:15:38 +0800

    change port

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const Koa = require('koa')
 const app = new Koa()
 const router = require('./router')
 router(app)
-app.listen(8082, () => {
-  console.log('8082')
+app.listen(8080, () => {
+  console.log('8080')
 })
